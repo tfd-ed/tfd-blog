adc13022ef2f6d5158d97af49ecd857631747fd4
KimAng <kimangkheang@gmail.com>
2022-03-19 11:31:46 +0900

    Basic structure

---
+++ pages/projects/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <section>
+    <div class="px-4 py-12 mx-auto">
+      <div class="max-w-4xl pt-24 mx-auto">
+        <div class="mb-24 text-left md:text-center">
+          <h1
+            class="mb-4 text-4xl font-bold leading-tight text-gray-900 md:text-5xl capitalize"
+          >
+            {{ $t("articles") }}
+          </h1>
+        </div>
+        <div class="relative">
+          <div class="absolute inset-0 flex items-center" aria-hidden="true">
+            <div class="w-full border-t border-black"></div>
+          </div>
+          <div class="relative flex justify-start">
+            <span class="pr-3 text-lg font-medium text-neutral-600 bg-white">
+              All Issues
+            </span>
+          </div>
+        </div>
+        <XyzTransitionGroup
+          appear-visible
+          class="flex flex-col space-y-16 lg:divide-y lg:divide-gray-100"
+          xyz="fade back-1 small-1 ease-out stagger-2 perspective-2"
+        >
+          <HorizontalCard :key="1"></HorizontalCard>
+          <HorizontalCard :key="2"></HorizontalCard>
+          <HorizontalCard :key="3"></HorizontalCard>
+          <HorizontalCard :key="4"></HorizontalCard>
+          <HorizontalCard :key="5"></HorizontalCard>
+          <HorizontalCard :key="6"></HorizontalCard>
+        </XyzTransitionGroup>
+      </div>
+    </div>
+  </section>
+</template>
+<script>
+import HorizontalCard from "@/components/card/horizontal-card";
+export default {
+  components: { HorizontalCard },
+};
+</script>
