dfd57bf830613e748c22320eb80d1b379818e556
KimAng <kimangkheang@gmail.com>
2022-03-22 22:31:54 +0900

    Dynamics Footer

--- components/footer/footer.vue
+++ components/footer/footer.vue
@@ -1,11 +1,11 @@
 <template>
   <footer class="footer p-10 bg-neutral text-neutral-content">
     <div>
-      <nuxt-link :to="localePath('index')"
+      <nuxt-link :to="localePath('/')"
         ><nuxt-img src="/tfd_logo.jpeg" class="w-12"></nuxt-img
       ></nuxt-link>
       <p>TFD<br />Teaching For Devs</p>
-      <p>info@tfdevs.com</p>
+      <p>{{ $config.EMAIL }}</p>
     </div>
     <div>
       <span class="footer-title">{{ $t("more") }}</span>
@@ -22,9 +22,7 @@
         <a href="https://t.me/tfdTech" target="_blank">
           <nuxt-img class="w-6" src="/telegram-svgrepo-com.svg"></nuxt-img>
         </a>
-        <a
-          href="https://www.youtube.com/c/TeachingForDevelopment"
-          target="_blank"
+        <a :href="$config.YOUTUBE_URL" target="_blank"
           ><svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -36,9 +34,7 @@
               d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"
             ></path></svg
         ></a>
-        <a
-          href="https://www.facebook.com/teachingfordevelopment"
-          target="_blank"
+        <a :href="$config.FACEBOOK_URL" target="_blank"
           ><svg
             xmlns="http://www.w3.org/2000/svg"
             width="24"
@@ -50,7 +46,7 @@
               d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
             ></path></svg
         ></a>
-        <a href="https://github.com/tfd-ed" target="_blank">
+        <a :href="$config.GITHUB_URL" target="_blank">
           <nuxt-img class="w-6" src="/github.png"></nuxt-img>
         </a>
       </div>
