94973f3da3035fe39b32e3de7386f146f2ce0088
KimAng <kimangkheang@gmail.com>
2022-11-28 14:50:56 +0900

    Disable Nuxt Content

---
+++ components/globals/maintenance.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="flex flex-col items-center justify-center max-w-lg">
+    <div class="mb-4">
+      <h1 class="text-5xl font-extrabold text-blue-600">503</h1>
+    </div>
+    <h3 class="mb-3 text-2xl font-bold text-center text-gray-700">
+      Temporarily down for maintenance We’ll be back soon!
+    </h3>
+    <p class="text-sm text-center text-gray-600">
+      Sorry for the inconvenience but we’re performing some maintenance at the
+      moment. If you need to you can always
+      <a class="text-blue-600 hover:underline">Contact us </a>, otherwise we’ll
+      be back online shortly! — The Team
+    </p>
+  </div>
+</template>
--- pages/tos.vue
+++ pages/tos.vue
@@ -1,53 +1,53 @@
 <template>
-  <section>
-    <div class="px-4 py-24 mb-20 text-left bg-gray-50 md:text-center">
-      <h1
-        class="mb-4 text-2xl font-bold leading-tight text-gray-900 md:text-5xl"
-      >
-        {{ tos.title }}
-      </h1>
-      <p class="text-base text-gray-500 md:text-lg">
-        {{ $t("last_update") }}: {{ $moment(tos.createdAt).format("LLLL") }}
-      </p>
-    </div>
-    <nuxt-content
-      class="px-4 pb-20 max-w-2xl md:max-w-4xl mx-auto prose text-justify"
-      :document="tos"
-    />
+  <section class="px-4 py-24 mx-auto max-w-7xl">
+    <LazyGlobalsMaintenance />
+    <!--      <h1-->
+    <!--        class="mb-4 text-2xl font-bold leading-tight text-gray-900 md:text-5xl"-->
+    <!--      >-->
+    <!--        {{ tos.title }}-->
+    <!--      </h1>-->
+    <!--      <p class="text-base text-gray-500 md:text-lg">-->
+    <!--        {{ $t("last_update") }}: {{ $moment(tos.createdAt).format("LLLL") }}-->
+    <!--      </p>-->
+
+    <!--    <nuxt-content-->
+    <!--      class="px-4 pb-20 max-w-2xl md:max-w-4xl mx-auto prose text-justify"-->
+    <!--      :document="tos"-->
+    <!--    />-->
   </section>
 </template>
 <script>
 export default {
-  async asyncData(context) {
-    const { $content, app } = context;
-    const defaultLocale = app.i18n.locale;
-    const tos = await $content(`${defaultLocale}/tos`).fetch();
-    return { tos };
-  },
-  head() {
-    return {
-      title: this.$config.SITE_TITLE + " | " + this.tos.title,
-      htmlAttrs: {
-        lang: this.$i18n.locale,
-      },
-      meta: [
-        {
-          hid: "og:description",
-          property: "og:description",
-          content: this.tos.description,
-        },
-        {
-          property: "og:title",
-          hid: "og:title",
-          content: this.$config.SITE_TITLE + " | " + this.tos.title,
-        },
-        {
-          hid: "og:image",
-          property: "og:image",
-          content: "/front_cover.png",
-        },
-      ],
-    };
-  },
+  // async asyncData(context) {
+  //   const { $content, app } = context;
+  //   const defaultLocale = app.i18n.locale;
+  //   const tos = await $content(`${defaultLocale}/tos`).fetch();
+  //   return { tos };
+  // },
+  // head() {
+  //   return {
+  //     title: this.$config.SITE_TITLE + " | " + this.tos.title,
+  //     htmlAttrs: {
+  //       lang: this.$i18n.locale,
+  //     },
+  //     meta: [
+  //       {
+  //         hid: "og:description",
+  //         property: "og:description",
+  //         content: this.tos.description,
+  //       },
+  //       {
+  //         property: "og:title",
+  //         hid: "og:title",
+  //         content: this.$config.SITE_TITLE + " | " + this.tos.title,
+  //       },
+  //       {
+  //         hid: "og:image",
+  //         property: "og:image",
+  //         content: "/front_cover.png",
+  //       },
+  //     ],
+  //   };
+  // },
 };
 </script>
