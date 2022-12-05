d8ebf2566c903dd200622bd1406ff86d46142d62
KimAng <kimangkheang@gmail.com>
2022-12-05 21:26:10 +0900

    Enable projects

--- pages/project/index.vue
+++ pages/project/index.vue
@@ -1,76 +1,76 @@
 <template>
   <div class="px-4 py-12 mx-auto h-screen">
-    <LazyGlobalsMaintenance />
-    <!--      <div class="max-w-6xl pt-24 mx-auto">-->
-    <!--        <div class="mb-6 text-left md:text-center">-->
-    <!--          <h1-->
-    <!--            class="mb-4 text-4xl font-bold leading-tight text-gray-900 md:text-5xl capitalize"-->
-    <!--          >-->
-    <!--            {{ $t("projects") }}-->
-    <!--          </h1>-->
-    <!--        </div>-->
-    <!--        <div class="relative">-->
-    <!--          <div class="absolute inset-0 flex items-center" aria-hidden="true">-->
-    <!--            <div class="w-full border-t border-black"></div>-->
-    <!--          </div>-->
-    <!--          <div class="relative flex justify-start">-->
-    <!--            <span class="pr-3 text-lg font-medium text-neutral-600 bg-white">-->
-    <!--              {{ $t("all_issues") }}-->
-    <!--            </span>-->
-    <!--          </div>-->
-    <!--        </div>-->
-    <!--        <client-only>-->
-    <!--          <XyzTransitionGroup-->
-    <!--            appear-visible-->
-    <!--            class="flex flex-col space-y-16 lg:divide-y lg:divide-gray-100"-->
-    <!--            xyz="fade back-1 small-80% ease-out stagger-2 perspective-2"-->
-    <!--          >-->
-    <!--            <LazyCardsHorizontalCard-->
-    <!--              v-for="(project, $index) in projects"-->
-    <!--              :key="`project-${$index}`"-->
-    <!--              :post="project"-->
-    <!--            ></LazyCardsHorizontalCard>-->
-    <!--          </XyzTransitionGroup>-->
-    <!--          <LazyCommonsConfused v-if="projects.length === 0">-->
-    <!--            <template #content>-->
-    <!--              <p class="mt-6 text-gray-500">-->
-    <!--                {{ $t("no_articles") }}-->
-    <!--              </p>-->
-    <!--            </template>-->
-    <!--          </LazyCommonsConfused>-->
-    <!--        </client-only>-->
-    <!--        <div v-if="nextPage" class="flex flex-row justify-center mx-auto mt-12">-->
-    <!--          <div class="btn-group">-->
-    <!--            <button class="btn">«</button>-->
-    <!--            <button class="btn">{{ $t("page") }} 1</button>-->
-    <!--            <nuxt-link class="btn" :to="localePath('/project/page/2')"-->
-    <!--              >»</nuxt-link-->
-    <!--            >-->
-    <!--          </div>-->
-    <!--        </div>-->
-    <!--      </div>-->
+    <!--    <LazyGlobalsMaintenance />-->
+    <div class="max-w-6xl pt-24 mx-auto">
+      <div class="mb-6 text-left md:text-center">
+        <h1
+          class="mb-4 text-4xl font-bold leading-tight text-gray-900 md:text-5xl capitalize"
+        >
+          {{ $t("projects") }}
+        </h1>
+      </div>
+      <div class="relative">
+        <div class="absolute inset-0 flex items-center" aria-hidden="true">
+          <div class="w-full border-t border-black"></div>
+        </div>
+        <div class="relative flex justify-start">
+          <span class="pr-3 text-lg font-medium text-neutral-600 bg-white">
+            {{ $t("all_issues") }}
+          </span>
+        </div>
+      </div>
+      <client-only>
+        <XyzTransitionGroup
+          appear-visible
+          class="flex flex-col space-y-16 lg:divide-y lg:divide-gray-100"
+          xyz="fade back-1 small-80% ease-out stagger-2 perspective-2"
+        >
+          <LazyCardsHorizontalCard
+            v-for="(project, $index) in projects"
+            :key="`project-${$index}`"
+            :post="project"
+          ></LazyCardsHorizontalCard>
+        </XyzTransitionGroup>
+        <LazyCommonsConfused v-if="projects.length === 0">
+          <template #content>
+            <p class="mt-6 text-gray-500">
+              {{ $t("no_articles") }}
+            </p>
+          </template>
+        </LazyCommonsConfused>
+      </client-only>
+      <div v-if="nextPage" class="flex flex-row justify-center mx-auto mt-12">
+        <div class="btn-group">
+          <button class="btn">«</button>
+          <button class="btn">{{ $t("page") }} 1</button>
+          <nuxt-link class="btn" :to="localePath('/project/page/2')"
+            >»</nuxt-link
+          >
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
 export default {
-  // async asyncData(context) {
-  //   const { $content, app } = context;
-  //   const defaultLocale = app.i18n.locale;
-  //   const tenProjects = await $content(`${defaultLocale}/project`, {
-  //     text: true,
-  //   })
-  //     .sortBy("createdAt", "desc")
-  //     .limit(10)
-  //     .fetch();
-  //   const nextPage = tenProjects.length === 10;
-  //   const projects = nextPage ? tenProjects.slice(0, -1) : tenProjects;
-  //   return {
-  //     projects: projects.map((project) => ({
-  //       ...project,
-  //       path: project.path.replace(`/${defaultLocale}`, ""),
-  //     })),
-  //     nextPage,
-  //   };
-  // },
+  async asyncData(context) {
+    const { $content, app } = context;
+    const defaultLocale = app.i18n.locale;
+    const tenProjects = await $content(`${defaultLocale}/project`, {
+      text: true,
+    })
+      .sortBy("createdAt", "desc")
+      .limit(10)
+      .fetch();
+    const nextPage = tenProjects.length === 10;
+    const projects = nextPage ? tenProjects.slice(0, -1) : tenProjects;
+    return {
+      projects: projects.map((project) => ({
+        ...project,
+        path: project.path.replace(`/${defaultLocale}`, ""),
+      })),
+      nextPage,
+    };
+  },
 };
 </script>
