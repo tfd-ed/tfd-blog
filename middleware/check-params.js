1500f038d225337f3c47fe5e33aa9a7869940b0f
Kheang Kimang <kimangkheang@gmail.com>
2023-12-28 21:42:57 +0700

    Update middleware/check-params.js
    Co-authored-by: tfd-ed <info@tfdevs.com>

--- middleware/check-params.js
+++ middleware/check-params.js
@@ -1,12 +1,7 @@
 export default function ({ query, redirect }) {
-  // Check if multiple query parameters are present
   const requiredParams = ["previous", "prefer", "name", "email"];
-
-  // Check if any required parameter is missing
   const missingParams = requiredParams.filter((param) => !query[param]);
-
   if (missingParams.length > 0) {
-    // Redirect to the home page
     redirect("/");
   }
 }
