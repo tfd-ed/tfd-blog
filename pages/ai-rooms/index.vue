b4349e70e7f2cc4a5e0bfa7777099c4dff7a488c
KimangMac14 <kimangkheang@gmail.com>
2024-09-19 15:12:59 +0700

    Fix Rooms

--- pages/ai-rooms/index.vue
+++ pages/ai-rooms/index.vue
@@ -23,34 +23,24 @@
           :path="item.path"
         >
           <template #icon>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="size-10 text-red-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path d="M12 14l9-5-9-5-9 5 9 5z" />
-              <path
-                d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-              />
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-              />
-            </svg> </template
+            <div class="bg-white items-center">
+              <img class="object-contain w-full h-40" :src="item.icon" />
+            </div> </template
         ></AiCard>
       </div>
 
       <div class="mt-12 text-center">
-        <nuxt-link
+        <div
+          class="inline-block rounded bg-tfd px-12 py-3 text-lg font-medium text-white transition hover:bg-red-700 focus:outline-none focus:ring focus:ring-black cursor-pointer"
+        >
+          {{ $t("enroll") }} Data Science & AI
+        </div>
+        <!-- <nuxt-link
           to="/bootcamps/data-science-ai"
           class="inline-block rounded bg-tfd px-12 py-3 text-lg font-medium text-white transition hover:bg-red-700 focus:outline-none focus:ring focus:ring-black"
         >
           {{ $t("enroll") }} Data Science & AI
-        </nuxt-link>
+        </nuxt-link> -->
       </div>
     </div>
   </section>
